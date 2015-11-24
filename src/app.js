41a32c2ef4c019f07d7fa70096686d62200c0eae
Sasitha Sonnadara <sasithas@leapset.com>
2015-11-24 09:52:04 +0530

    moved to react starter kit

--- src/app.js
+++ src/app.js
@@ -1,38 +1,97 @@
-/**
- * Created by wathmal on 10/29/15.
- */
+/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
 
-import TwitterScraper from './api/TwitterScraper';
-import TweetStore from './api/TweetStore';
+import 'babel-core/polyfill';
+import ReactDOM from 'react-dom';
+import FastClick from 'fastclick';
+import Router from './routes';
+import Location from './core/Location';
+import { addEventListener, removeEventListener } from './utils/DOMUtils';
 
-var express= require('express');
+let cssContainer = document.getElementById('css');
+const appContainer = document.getElementById('app');
+const context = {
+  onSetTitle: value => document.title = value,
+  onSetMeta: (name, content) => {
+    // Remove and create a new <meta /> tag in order to make it work
+    // with bookmarks in Safari
+    const elements = document.getElementsByTagName('meta');
+    [].slice.call(elements).forEach((element) => {
+      if (element.getAttribute('name') === name) {
+        element.parentNode.removeChild(element);
+      }
+    });
+    const meta = document.createElement('meta');
+    meta.setAttribute('name', name);
+    meta.setAttribute('content', content);
+    document.getElementsByTagName('head')[0].appendChild(meta);
+  },
+};
 
+function render(state) {
+  Router.dispatch(state, (newState, component) => {
+    ReactDOM.render(component, appContainer, () => {
+      // Restore the scroll position if it was saved into the state
+      if (state.scrollY !== undefined) {
+        window.scrollTo(state.scrollX, state.scrollY);
+      } else {
+        window.scrollTo(0, 0);
+      }
 
+      // Remove the pre-rendered CSS because it's no longer used
+      // after the React app is launched
+      if (cssContainer) {
+        cssContainer.parentNode.removeChild(cssContainer);
+        cssContainer = null;
+      }
+    });
+  });
+}
 
-var app= express();
+function run() {
+  let currentLocation = null;
+  let currentState = null;
 
-const username= 'BApikz';
+  // Make taps on links and buttons work fast on mobiles
+  FastClick.attach(document.body);
 
+  // Re-render the app when window.location changes
+  const unlisten = Location.listen(location => {
+    currentLocation = location;
+    currentState = Object.assign({}, location.state, {
+      path: location.pathname,
+      query: location.query,
+      state: location.state,
+      context,
+    });
+    render(currentState);
+  });
 
-let store= new TweetStore(username);
-//let scraper = new TwitterScraper(username, store);
-//scraper.getRecentTweets();
+  // Save the page scroll position into the current location's state
+  const supportPageOffset = window.pageXOffset !== undefined;
+  const isCSS1Compat = ((document.compatMode || '') === 'CSS1Compat');
+  const setPageOffset = () => {
+    currentLocation.state = currentLocation.state || Object.create(null);
+    if (supportPageOffset) {
+      currentLocation.state.scrollX = window.pageXOffset;
+      currentLocation.state.scrollY = window.pageYOffset;
+    } else {
+      currentLocation.state.scrollX = isCSS1Compat ?
+        document.documentElement.scrollLeft : document.body.scrollLeft;
+      currentLocation.state.scrollY = isCSS1Compat ?
+        document.documentElement.scrollTop : document.body.scrollTop;
+    }
+  };
 
+  addEventListener(window, 'scroll', setPageOffset);
+  addEventListener(window, 'pagehide', () => {
+    removeEventListener(window, 'scroll', setPageOffset);
+    unlisten();
+  });
+}
 
-store.getTweetsFromCollection(10,10).then(docs => {
-    docs.forEach(tweet => {
-        console.log(JSON.stringify(tweet.text));
-    })
-});
-
-
-
-/*
-
-app.get('/', function(req, res){
-
-    res.json({status: "doneeee na ne :P"});
-});
-*/
-
-export default app;
+// Run the application when both DOM is ready and page content is loaded
+if (['complete', 'loaded', 'interactive'].includes(document.readyState) && document.body) {
+  run();
+} else {
+  document.addEventListener('DOMContentLoaded', run, false);
+}
--- src/server.js
+++ src/server.js
@@ -1,9 +1,58 @@
+/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
 
-// var app= require('./app');
-import app from './app';
-let server= app.listen(3000, function () {
-    var host= server.address().address;
-    var port= server.address().port;
+import 'babel-core/polyfill';
+import path from 'path';
+import express from 'express';
+import React from 'react';
+import ReactDOM from 'react-dom/server';
+import Router from './routes';
+import Html from './components/Html';
 
-    console.log('app is running at http://%s:%s', host, port);
+const server = global.server = express();
+const port = process.env.PORT || 5000;
+server.set('port', port);
+
+//
+// Register Node.js middleware
+// -----------------------------------------------------------------------------
+server.use(express.static(path.join(__dirname, 'public')));
+
+//
+// Register API middleware
+// -----------------------------------------------------------------------------
+server.use('/api/content', require('./api/content'));
+
+//
+// Register server-side rendering middleware
+// -----------------------------------------------------------------------------
+server.get('*', async (req, res, next) => {
+  try {
+    let statusCode = 200;
+    const data = { title: '', description: '', css: '', body: '' };
+    const css = [];
+    const context = {
+      onInsertCss: value => css.push(value),
+      onSetTitle: value => data.title = value,
+      onSetMeta: (key, value) => data[key] = value,
+      onPageNotFound: () => statusCode = 404,
+    };
+
+    await Router.dispatch({ path: req.path, context }, (state, component) => {
+      data.body = ReactDOM.renderToString(component);
+      data.css = css.join('');
+    });
+
+    const html = ReactDOM.renderToStaticMarkup(<Html {...data} />);
+    res.status(statusCode).send('<!doctype html>\n' + html);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//
+// Launch the server
+// -----------------------------------------------------------------------------
+server.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`The server is running at http://localhost:${port}/`);
 });
