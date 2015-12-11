85c4c9a13e2cf4c70e1b97fa1fbb1549e1cd7da8
Sasitha Sonnadara <sasithas@leapset.com>
2015-12-12 00:50:23 +0530

    added mongoose

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -21,10 +21,13 @@ class Navigation extends Component {
         </a>
         <ul className="nav navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="#">dashboard <span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="#">dashboard<span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">about</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">source</a>
           </li>
         </ul>
         <form className="form-inline navbar-form pull-right">
